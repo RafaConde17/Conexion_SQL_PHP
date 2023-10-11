ffdfa067900b5c12cb09ea10a69c0c70e0506d00
Rafael Conde <115685888+RafaConde17@users.noreply.github.com>
2023-10-11 02:13:42 -0500

    Carga inicial

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+include_once('Conexion/conexion.php'); // Ajusta la ruta según la ubicación real de conexion.php
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conexion php + SQL Server</title>
+</head>
+<body>
+Hola    
+</body>
+</html>
